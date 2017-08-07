338e5b71ae844d1ae5dfaf4c418fdf2bcdfafc0d
allgreed <olixem@gmail.com>
2017-08-07 15:56:30 +0200

    Zoom is dependant on state

--- src/store/getters.js
+++ src/store/getters.js
@@ -26,9 +26,17 @@ export default
     {
         return { lat: state.location[0], lng: state.location[1] };
     },
-    zoom (state)
+    zoom ()
     {
-        return state.zoom;
+        // zoom will determinate how far from user we are looking
+        // for shop or how far is shop from the user
+        // for example user is looking around 1 km of his coords
+        // so zoom needs to be calculated to show client and shop
+        // both on the map, or if user is searching from unknown or
+        // far location zoom will be set to comfortably view the Map
+
+        // this is only temporrary
+        return 15;
     },
     searchLocationFieldVisib (state)
     {
