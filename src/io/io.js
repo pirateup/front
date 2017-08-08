78503aa63aa72013f3fb422c4e8364db20d1106f
allgreed <olixem@gmail.com>
2017-08-09 00:23:29 +0200

    Added interceptor to prevent further boilerplate code

--- src/io/io.js
+++ src/io/io.js
@@ -1,11 +1,5 @@
 import axios from 'axios';
 
-axios.interceptors.response.use(
-    undefined,
-    // passes error down the Promise chain
-    error => Promise.reject(error),
-);
-
 const baseURL = process.env.BACKEND_URL;
 
 const HTTP = axios.create({
@@ -13,10 +7,14 @@ const HTTP = axios.create({
     headers: {},
 });
 
+HTTP.interceptors.response.use(
+    response => response.data,
+    error => Promise.reject(error), // "rethrows" error
+);
+
 // Remove this and below line after adding another function to the io
 /* eslint-disable */
 export function requestNearbyShops (location)
 {
-    return HTTP.get(`nearby?latitude=${location.latitude}&longitude=${location.longitude}`)
-    .then(response => response.data);
+    return HTTP.get(`nearby?latitude=${location.latitude}&longitude=${location.longitude}`);
 }
