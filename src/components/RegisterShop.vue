a0416edeb88b8e4f93fe7f53e762cecea8aa2609
allgreed <olixem@gmail.com>
2017-08-13 20:16:13 +0200

    Component stub

---
+++ src/components/RegisterShop.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="register-form">
+        <form>
+
+        </form>
+   </div>
+</template>
+
+<script>
+export default
+{
+    name: 'register-shop',
+    computed:
+    {
+
+    },
+    methods:
+    {
+
+    },
+};
+</script>
+
+<style scoped>
+
+
+</style>
