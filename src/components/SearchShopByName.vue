b16fa499c88771ef019942db790f44c48701e606
allgreed <olixem@gmail.com>
2017-08-21 14:54:26 +0200

    Added a stub

---
+++ src/components/SearchShopByName.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="search-by-name">
+        <span>Testing test</span>
+   </div>
+</template>
+
+<script>
+
+export default
+{
+    name: 'search-shop-by-name',
+    data ()
+    {
+        return {
+
+        };
+    },
+    computed:
+    {
+
+    },
+    methods:
+    {
+
+    },
+};
+</script>
+
+<style scoped>
+
+
+</style>
