c3ae37a7f564af51c8e9d6fd73add4bbf2bc2330
allgreed <olixem@gmail.com>
2017-09-01 11:42:13 +0200

    Typo

--- src/store/state.js
+++ src/store/state.js
@@ -5,6 +5,6 @@ export default
         { id: 'idSetByServer1234567', name: 'U Jadźki', distance: 300, street: 'Ketonowa 7', img: 'https://s-media-cache-ak0.pinimg.com/736x/a3/03/61/a30361985814a564e8b87515bb373661--shop-facade-coffeeshops.jpg', location: [51.3668, -0.4628] },
         { id: 'idSetByServer7654321', name: 'U Józka', distance: 500, street: 'Betonowa 9', img: 'https://s-media-cache-ak0.pinimg.com/736x/a3/03/61/a30361985814a564e8b87515bb373661--shop-facade-coffeeshops.jpg', location: [51.4694, -0.08396] },
     ],
-    // shall be refactored to an object insted of an array in all other places
+    // shall be refactored to an object instead of an array in all other places
     location: { latitude: 52.23, longitude: 21.02 },
 };
